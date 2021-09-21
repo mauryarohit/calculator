7ac0ddce0510d35abc3b787b753561f6bfddff84
Rohit Maurya <maurya.rohit704@gmail.com>
2021-09-21 18:20:48 +0530

    corrected failing tests for single number

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,9 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
     public int add(String numbers) {
         if(numbers.equals("")) {
             return 0;
         }
+        List<String> nums = Arrays.asList(numbers.split(","));
+        if(nums.size() == 1) {
+            return Integer.parseInt(nums.get(0));
+        }
+
         return -1;
     }
 
