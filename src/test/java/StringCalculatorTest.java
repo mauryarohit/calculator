a73888d3895cdd84bacd177d117206d2d8daecb9
Rohit Maurya <maurya.rohit704@gmail.com>
2021-09-21 20:14:41 +0530

    Test for counting number of calls

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -4,54 +4,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
 
+    StringCalculator stringCalculator = new StringCalculator();
+
     @Test
     public void addForEmptyString() {
-        StringCalculator stringCalculator = new StringCalculator();
         int actual = stringCalculator.add("");
         assertEquals(0, actual, "Should return 0 for empty string.");
     }
 
     @Test
     public void addForASingleNumber() {
-        StringCalculator stringCalculator = new StringCalculator();
         int actual = stringCalculator.add("1");
         assertEquals(1, actual, "Should handle single number.");
     }
 
     @Test
     public void addForTwoNumbers() {
-        StringCalculator stringCalculator = new StringCalculator();
         int actual = stringCalculator.add("1,2");
         assertEquals(3, actual, "Should add two numbers");
     }
 
     @Test
     public void addForMoreThanTwoNumbers() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("1,2,3"));
     }
 
     @Test
     public void addForNewLine() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(5, stringCalculator.add("1\n2,3"));
     }
 
     @Test
     public void addSupportForDelimiter() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(4, stringCalculator.add("\\;\n1;3"));
     }
 
     @Test
     public void addForNegativeNumberThrowsException() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("1,-1"));
     }
 
     @Test
     public void addForMultipleNegatives() {
-        StringCalculator stringCalculator = new StringCalculator();
         String message = assertThrows(IllegalArgumentException.class, () -> {
             stringCalculator.add("-1,-1");
         }).getMessage();
@@ -60,26 +54,27 @@ public class StringCalculatorTest {
 
     @Test
     public void addForNumbersBiggerThanThousand() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(2, stringCalculator.add("2,1000"),"Numbers bigger than 1000 should be ignored.");
     }
 
     @Test
     public void addForDifferentSizeDelimiter() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(3, stringCalculator.add("//[***]\n1***2"));
     }
 
     @Test
     public void addForMultipleDelimiter() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(3, stringCalculator.add("//[*][%]\n1%2"));
     }
 
     @Test
     public void addForMultipleDelimiterWithDifferentSize() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(3, stringCalculator.add("//[**][%%]\n1%%2"));
+    }
+
+    @Test
+    public void getCalledCountTest() {
+        assertEquals(12, stringCalculator.getCalledCount());
     }
 
 }
