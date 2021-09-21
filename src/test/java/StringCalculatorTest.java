94ac9f686531d8ba01de80ff94ac17a3f1007198
Rohit Maurya <maurya.rohit704@gmail.com>
2021-09-21 18:16:55 +0530

    added test for single number input

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -11,4 +11,11 @@ public class StringCalculatorTest {
         assertEquals(0, actual, "Should return 0 for empty string.");
     }
 
+    @Test
+    public void addForASingleNumber() {
+        StringCalculator stringCalculator = new StringCalculator();
+        int actual = stringCalculator.add("1");
+        assertEquals(1, actual, "Should handle single number.");
+    }
+
 }
